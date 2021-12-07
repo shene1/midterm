72a5c98261e659a15de03a937e9f0f5ac2dff67f
Shene rashid <shene.rashid999@gmail.com>
2021-12-07 15:14:56 +0300

    i create my project

---
+++ lib/models/note.dart
@@ -0,0 +1,11 @@
+//! Define the model class Note here
+import 'package:flutter/material.dart';
+
+class Note {
+  String title;
+  String content;
+  String description;
+  Color noteColor;
+
+  Note(this.title, this.content, this.description, this.noteColor);
+}
